05d85f748a3bb7ae8c974f1501e50db57a001185
Sean <seanchen11235@gmail.com>
2017-05-11 16:53:05 -0700

    Update README, config, and add test input

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,12 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :elixir_mapreduce, 
+  input_dir: "../test/input",
+  output_dir: "../test/output",
+  n_file_splits: 50,
+  n_output_files: 5
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
